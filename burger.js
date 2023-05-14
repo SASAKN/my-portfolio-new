f1b5d2ae0c70ef40957b2faef35d728c1b267a95
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-05-14 18:19:10 +0900

    いらなくなった動作定義ファイルを削除。

--- burger.js
+++ burger.js
@@ -1,28 +1,4 @@
-//変数の設定
-const elem = document.getElementById('smenu');
-let burger = 1;
-//動作の設定
-function menuopen() {
-    elem.className = 'animactive';
-}
-function menuclose() {
-    elem.className = 'animclose1';
-}
 document.querySelector('.burger').addEventListener('click', function (e) {
     e.preventDefault;
     this.className = this.className === 'burger' ? 'burger on' : 'burger';
-});
-document.querySelector('.burger').addEventListener('click', function (e) {
-    switch (burger) {
-        case 1:
-            menuopen();
-            burger++;
-            console.log('Animation:メニューをオープンしました。');
-            break;
-        case 2:
-            menuclose();
-            burger = 1;
-            console.log('Animation:メニューのストップを実行しました。');
-            break;
-    }
 });
--- load.js
+++
@@ -1,6 +0,0 @@
-setTimeout(() => {
-    document.getElementById('load').style.animation = 'stop 0.5s forwards ease-in-out';
-}, 3000);
-setTimeout(() => {
-    document.getElementById('load').remove();
-}, 3500);
