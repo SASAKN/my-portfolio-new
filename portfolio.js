986bb3d2e57f327970cd96733f865b4992661e52
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-05-26 18:31:40 +0900

    Update

--- portfolio.js
+++ portfolio.js
@@ -1,20 +1,7 @@
+//変更禁止！
 document.querySelector('.burger').addEventListener('click', function (e) {
     e.preventDefault;
     this.className = this.className === 'burger' ? 'burger on' : 'burger';
 });
-//リンク先指定
-document.querySelectorAll('.link')[0].addEventListener('click', function(){
-    location.href = 'https://sites.google.com/view/nksteam-os/%E3%83%9B%E3%83%BC%E3%83%A0';
-});
-document.querySelectorAll('.link')[1].addEventListener('click', function(){
-    location.href = '#dolphin';
-});
-//スクロール時のふわふわ
-document.addEventListener('scroll', function(){
-    var scroll = window.pageYOffset;
-    if (scroll > 983){
-        alert('スクロールされた');
-    }else{
-    
-    }
-});
+
+//スクロールアクションの設定
